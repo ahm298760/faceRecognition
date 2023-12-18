1f56a13ea6c9e2709264a5d251b3efc67fbb07ae
FillCaverMix <115676993+ahm298760@users.noreply.github.com>
2023-12-18 09:51:39 +0200

    Add files via upload

---
+++ data/data.js
@@ -0,0 +1,34 @@
+var persons = [
+    {
+        name : "Ahmed Mohamed",
+        Img : "data/image/Ahmed_Mohamed.jpg"
+    },
+    {
+        name : "Zaid Mohamed",
+        Img : "data/image/Zaid_Mohamed.jpg"
+    },
+    {
+        name : "Ahmed Sameih",
+        Img : "data/image/Ahmed_Sameih.jpg"
+    },
+    {
+        name : "Hossam Hamdy",
+        Img : "data/image/Hossam_Hamdy.jpg"
+    },
+    {
+        name : "Hazem Mohamed",
+        Img : "data/image/Hazem_Mohamed.jpg"
+    },/*
+    {
+        name : "Mirona Emad",
+        Img : "data/image/Mirona_Emad.jpg"
+    },
+    {
+        name : "Mayar Mohamed",
+        Img : "data/image/Mayar_Mohamed.jpg"
+    },*/
+    {
+        name : "Mennatuallah Ahmed",
+        Img : "data/image/Mennatuallah_Ahmed.jpg"
+    },
+];
